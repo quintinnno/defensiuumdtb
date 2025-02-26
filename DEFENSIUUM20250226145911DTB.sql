725ab8087f2a3d47e6e1c50dcd0420dbfe534c62
José Quintinno <josequintino@hotmail.com.br>
2025-02-26 15:21:47 -0300

    🔵 feature: Implementar modelagem para novas funcionalidades

--- DEFENSIUUM20250226145911DTB.sql
+++ DEFENSIUUM20250226145911DTB.sql
@@ -4,6 +4,9 @@
     Autor: Desenvolvimento
 */
 
+drop table if exists tb_usuario cascade;
+drop table if exists tb_usuario_categoria cascade;
+
 create table if not exists tb_usuario_categoria (
     code bigint not null auto_increment,
     descricao varchar (80) not null,
@@ -13,6 +16,11 @@ create table if not exists tb_usuario_categoria (
 create table if not exists tb_usuario (
     code bigint not null auto_increment,
     id_usuario_categoria bigint not null,
+    id_pessoa bigint not null,
+    identificador varchar (100) not null,
+    senha varchar (255) not null,
+    data_hora_criacao timestamp not null default current_timestamp,
+    e_ativo boolean not null default true,
     constraint pk_usuario primary key (code),
     constraint fk_usuario_categoria foreign key (id_usuario_categoria) references tb_usuario_categoria (code)
 );
